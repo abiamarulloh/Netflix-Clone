56b8db413be90d47d7f7fa65b91b27a99c84b7de
abi amarulloh <abiamarulloh06@gmail.com>
2022-06-01 19:29:52 +0700

    Creating MainTabBarViewController

---
+++ Netflix Clone/UpcomingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by abi amarulloh on 01/06/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
